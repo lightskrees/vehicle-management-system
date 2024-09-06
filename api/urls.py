1c17b70e0e1faa797d7fad46ff9faaa8a97370b5
lightskrees <nchris3010@gmail.com>
2024-09-06 14:01:46 +0200

    changed the token default view.

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from rest_framework_simplejwt.views import TokenRefreshView
+from api.views import TokenPairView
 
 urlpatterns = [
     path('auth/', include('rest_framework.urls')),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', TokenPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
