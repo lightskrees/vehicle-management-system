3b0e32b5eca904b3db3692e7d0c5921c559ad61c
lightskrees <nchris3010@gmail.com>
2024-09-06 14:00:55 +0200

    set TokenPairView for the given tokens.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from api.serializers import TokenSerializer
+
+
+class TokenPairView(TokenObtainPairView):
+    serializer_class = TokenSerializer
