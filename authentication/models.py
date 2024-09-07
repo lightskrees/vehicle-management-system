4cd6aa9e860e66139322ec8f594647bd85585568
lightskrees <nchris3010@gmail.com>
2024-09-07 13:29:02 +0200

    fixed an import bug

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 from django.utils.translation import gettext as _
 
 from authentication.managers import CustomUserManager
