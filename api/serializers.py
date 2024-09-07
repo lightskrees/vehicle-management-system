24eb37f952cde7dc8d3bad46c93e64dd78dd634d
lightskrees <nchris3010@gmail.com>
2024-09-07 14:21:38 +0200

    altered response data from token serializer

--- api/serializers.py
+++ api/serializers.py
@@ -6,17 +6,20 @@ class TokenSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token_data = super().get_token(user)
 
-        # Add custom claims
-        token_data['email'] = user.email
+        token_data['first_name'] = user.first_name
+        token_data['last_name'] = user.last_name
+        token_data['username'] = user.email
 
         return token_data
 
     def validate(self, attrs):
         token_data = super().validate(attrs)
 
-        user_data = {
+        user_details = {
                 "user_id": self.user.id,
-                "email": self.user.email
+                "username": self.user.email,
+                "first_name": self.user.first_name,
+                "last_name": self.user.last_name,
         }
-        data = {"user": user_data, **token_data}
+        data = {"user": user_details, **token_data}
         return data
