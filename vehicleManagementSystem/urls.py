81d46d0dc4486da9240e7b692105377b58dd570b
thedude <nchris3010@gmail.com>
2024-09-13 16:18:13 +0200

    removal of unused import

--- vehicleManagementSystem/urls.py
+++ vehicleManagementSystem/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from pickletools import name2i
-
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("api.urls"), name="api")]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include("api.urls"))]
