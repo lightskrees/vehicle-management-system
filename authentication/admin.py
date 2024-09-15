b0c1efac8dabc56c3af0feebc163ef7d042d2eb3
thedude <nchris3010@gmail.com>
2024-09-15 12:44:16 +0200

    configurations for the admin site

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from authentication.models import AppUser, Driver
+
+admin.site.register(AppUser)
+admin.site.register(Driver)
+
+
 # Register your models here.
--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from management.models import Vehicle, VehicleDriverAssignment, VehicleTechnician
+
+admin.site.register(Vehicle)
+admin.site.register(VehicleDriverAssignment)
+admin.site.register(VehicleTechnician)
