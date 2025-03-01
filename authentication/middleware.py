27ec9f65c66d2d1a3de5cce4487ac1366aa46bfb
lightskrees <nchris3010@gmail.com>
2025-03-01 16:05:22 +0200

    configured token configuration through middlewares

---
+++ authentication/middleware.py
@@ -0,0 +1,15 @@
+# middleware.py
+from django.utils.deprecation import MiddlewareMixin
+
+
+class BearerTokenMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        auth_header = request.headers.get("Authorization")
+        if auth_header and not auth_header.startswith("Bearer "):
+            request.META["HTTP_AUTHORIZATION"] = f"Bearer {auth_header}"
+
+        response = self.get_response(request)
+        return response
