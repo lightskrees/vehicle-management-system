4301d193f60644b5c6a3f6a13542004445ed8079
lightskrees <nchris3010@gmail.com>
2025-04-06 13:18:41 +0200

    pagination configuration

--- vehicleManagementSystem/settings/main_settings.py
+++ vehicleManagementSystem/settings/main_settings.py
@@ -67,6 +67,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "authentication.authentication_conf.AuthenticationView",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
