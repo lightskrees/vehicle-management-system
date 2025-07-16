8ed4847799c438594c5e728f43277fbe2cd010c1
lightskrees <nchris3010@gmail.com>
2025-07-16 13:52:00 +0200

    updated `create_clone_in_financial_records` signal to handle updates.

--- vehicleBudget/signals.py
+++ vehicleBudget/signals.py
@@ -3,9 +3,9 @@ from django.db.models.signals import post_save
 from vehicleBudget.models import DocumentCost, FinancialRecord, FuelConsumption, VehicleMaintenance
 
 
-def create_clone_in_financial_records(sender, instance, created, **kwargs):
-    if not created and not sender.__name__ == "VehicleMaintenance":
-        return
+def create_clone_in_financial_records(sender, instance: VehicleMaintenance, created, **kwargs):
+    # if not created and not sender.__name__ == "VehicleMaintenance":
+    #     return
 
     payment_details = {
         "cost": instance.payment_amount,
@@ -14,11 +14,25 @@ def create_clone_in_financial_records(sender, instance, created, **kwargs):
     }
 
     if sender.__name__ == "DocumentCost":
-        FinancialRecord.objects.create(document_cost=instance, **payment_details)
+        if created:
+            FinancialRecord.objects.create(document_cost=instance, **payment_details)
+        else:
+            FinancialRecord.objects.filter(id=instance.id).update(document_cost=instance, **payment_details)
+
     elif sender.__name__ == "FuelConsumption":
-        FinancialRecord.objects.create(fuel_consumption=instance, **payment_details)
-    elif sender.__name__ == "VehicleMaintenance" and instance.status == sender.Status.APPROVED:
-        FinancialRecord.objects.create(vehicle_maintenance=instance, **payment_details)
+        if created:
+            FinancialRecord.objects.create(fuel_consumption=instance, **payment_details)
+        else:
+            FinancialRecord.objects.filter(id=instance.id).update(fuel_consumption=instance, **payment_details)
+    elif (
+        sender.__name__ == "VehicleMaintenance"
+        and instance.status == sender.Status.APPROVED
+        and instance.maintenance_end_date
+    ):
+        if created:
+            FinancialRecord.objects.create(vehicle_maintenance=instance, **payment_details)
+        else:
+            FinancialRecord.objects.filter(id=instance.id).update(vehicle_maintenance=instance, **payment_details)
 
 
 post_save.connect(create_clone_in_financial_records, sender=DocumentCost)
