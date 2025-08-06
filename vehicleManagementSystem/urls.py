8611ff7e72a70371968f11522b9b60dee447ed66
lightskrees <nchris3010@gmail.com>
2025-08-06 02:05:29 +0200

    media URL support.

--- vehicleManagementSystem/urls.py
+++ vehicleManagementSystem/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 admin.site.site_header = "System Administration"
 
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("api.urls")), path("", include("schemas"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("", include("schemas")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
